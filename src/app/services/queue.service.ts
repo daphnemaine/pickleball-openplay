d3f3024b733504988cf4f16bcd7adb3caecd9a04
Daphne Joyce Comendador <daphnejoycecomendador@192.168.1.13>
2026-06-28 12:32:17 +0800

    Created Queue Service

--- src/app/services/queue.service.ts
+++ src/app/services/queue.service.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Player } from '../models/player';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class QueueService {
+  private queueSubject = new BehaviorSubject<Player[]>([]);
+  public queue$ = this.queueSubject.asObservable();
+
+  addPlayer(player: Player): void {
+    const currentQueue = this.queueSubject.getValue();
+    this.queueSubject.next([...currentQueue, player]);
+  }
   
+  removePlayer(playerId: string): void {
+    this.queueSubject.next(
+      this.queueSubject.value.
+      filter(player => player.id !== playerId)  
+    );
+  }
+
+  getNextPlayers(count = 4): Player[] {
+    return this.queueSubject.value.slice(0, count);
+  }
+
+  dequeuePlayers(count = 4): void {
+    this.queueSubject.next(
+      this.queueSubject.value.slice(count)
+    );
+  }
 }
